a79e5eab2b8ac83423eeba4731b0a353b52b067b
woongjoonchoi <oongjoon10@gmail.com>
2022-02-21 17:15:01 +0900

    feat Nuemric
    Sets operation vs method

---
+++ Sets/set_method.py
@@ -0,0 +1,20 @@
+x= set('abced')
+y= set('hello')
+
+
+z = x.intersection(y)
+print(z)
+
+z.add('SPAM')
+
+print(z)
+
+z.update(set(['ac','bc']))
+print(z)
+
+z.remove('bc')
+
+print(z)
+
+for item in "cdf" :
+    print(item * 3 )
---
+++ Sets/set_op.py
@@ -0,0 +1,16 @@
+x= set('abced')
+y= set('hello')
+
+print(x)
+
+print(x-y)
+
+print(x|y)
+
+print(x & y)
+
+print(x ^ y )
+
+print(x>y , y<x)
+
+print( 'e' in x)

cd819b716d3ac0d955726592a54965572b00d132
woongjoonchoi <oongjoon10@gmail.com>
2023-11-24 23:49:34 +0900

    feat Numeric/ComplexNumber/img.py
    post about imagine number

---
+++ Numeric/ComplexNumber/img.py
@@ -0,0 +1,36 @@
+print(3+5j)
+
+print(type(3+5j))
+
+b= 3.15j
+print(b)
+print(type(b))
+
+b = 10.j
+
+print(b)
+print(type(b))
+
+b = 10j
+
+print(b)
+print(type(b))
+
+b=.001j
+print(b)
+print(type(b))
+
+
+b=1e100j
+print(b)
+print(type(b))
+
+
+b=3.14e-10j
+print(b)
+print(type(b))
+
+b=3.14_15_93j
+
+print(b)
+print(type(b))
